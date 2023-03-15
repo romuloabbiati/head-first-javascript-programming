24cd63003bedf94d5a781e54fd7e755e152b78e3
romuloabbiati <rhabbiati@hotmail.com>
2023-03-15 08:08:53 -0300

    Changing the stats msg

--- ch2/battleship/battleship.js
+++ ch2/battleship/battleship.js
@@ -29,4 +29,7 @@ while(!isSunk) {
   }
 }
 
-alert(`Your score is ${guesses} out of 7`);
+var stats = "You took " + guesses + " guesses to sink the battleship, " + 
+		"which means your shooting accuracy was " + (3/guesses);
+
+alert(stats);
