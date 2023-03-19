8ae4c9fd46663a6f2ccc113d0ff82752c0199a9b
romuloabbiati <rhabbiati@hotmail.com>
2023-03-19 15:15:37 -0300

    brain power

---
+++ ch3/brainPower/brainPower.js
@@ -0,0 +1,8 @@
+function doIt(param) {
+  param = 2;
+  console.log(param);
+}
+
+var test = 1;
+doIt(test);
+console.log(test);
---
+++ ch3/codeMagnets/codeMagnets.js
@@ -0,0 +1,13 @@
+function whatShallWear(temp) {
+  if(temp < 60) {
+    console.log("Wear a jacket");
+  } else if(temp < 70) {
+    console.log("Wear a sweater");
+  } else {
+    console.log("Wear a t-shirt");
+  }
+}
+
+whatShallWear(50);
+whatShallWear(80);
+whatShallWear(60);
