0f9f5dc0c25b30abe684782b9bad869a9062b7dd
romuloabbiati <rhabbiati@hotmail.com>
2023-05-27 08:33:15 +0100

    ship magnets

--- ch8/battleship/battleship.js
+++ ch8/battleship/battleship.js
@@ -14,9 +14,19 @@ var view = {
 }
 
 
-view.displayMiss('00');
+view.displayHit('06');
+view.displayMiss('13');
+view.displayHit('24');
+view.displayMiss('41');
+view.displayHit('10');
 view.displayHit('34');
-view.displayMiss('55');
-view.displayHit('12');
-view.displayMiss('25');
+view.displayMiss('56');
+view.displayMiss('01');
 view.displayHit('26');
+view.displayHit('11');
+view.displayHit('12');
+view.displayHit('44');
+view.displayHit('16');
+
+
+view.displayMessage('Tap tap, is this thing on?');
