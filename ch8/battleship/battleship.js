f16a8f651212bb524435ba353a027c5628a98884
romuloabbiati <rhabbiati@hotmail.com>
2023-06-06 07:38:11 +0100

    init(), handleFireButton, handleKeyPress

--- ch8/battleship/battleship.js
+++ ch8/battleship/battleship.js
@@ -91,22 +91,48 @@ function parseGuess(guess) {
   return null;
 }
 
+
+function init() {
+  var fireButton = document.getElementById('fireButton');
+  fireButton.onclick = handleFireButton;
+  var guessInput = document.getElementById('guessInput');
+  guessInput.onkeypress = handleKeyPress;
+}
+
+function handleFireButton() {
+  var guessInput = document.getElementById('guessInput');
+  var guess = guessInput.value;
+  controller.processGuess(guess);
+
+  guessInput.value = "";
+}
+
+function handleKeyPress(e) {
+  var fireButton = document.getElementById('fireButton');
+  if(e.keyCode === 13) {
+    fireButton.click();
+    return false;
+  }
+}
+
+window.onload = init;
+
 // console.log(parseGuess("A0"));
 // console.log(parseGuess("B6"));
 // console.log(parseGuess("G3"));
 // console.log(parseGuess("H0"));
 // console.log(parseGuess("A7"));
 
-controller.processGuess("A0");
+// controller.processGuess("A0");
 
-controller.processGuess("A6");
-controller.processGuess("B6");
-controller.processGuess("C6");
+// controller.processGuess("A6");
+// controller.processGuess("B6");
+// controller.processGuess("C6");
 
-controller.processGuess("C4");
-controller.processGuess("D4");
-controller.processGuess("E4");
+// controller.processGuess("C4");
+// controller.processGuess("D4");
+// controller.processGuess("E4");
 
-controller.processGuess("B0");
-controller.processGuess("B1");
-controller.processGuess("B2");
+// controller.processGuess("B0");
+// controller.processGuess("B1");
+// controller.processGuess("B2");
